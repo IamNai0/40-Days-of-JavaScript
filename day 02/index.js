effd72e536dfe721d55b8695b741bcca82104c85
IamNai0 <nehalasadesmail0@gmail.com>
2025-08-12 21:51:33 +0600

    Day 02 learing of JS

---
+++ day 02/index.js
@@ -0,0 +1,10 @@
+console.log("Day 02");
+
+//variables> Variable is a container for a value in JS
+// syntax: let/const storageName = value;
+let fruit = "apple"; //string
+fruit = "banana"; //reassigning value
+let vegetable = "carrot"; //new variable string
+
+fruit = vegetable; //reassigning fruit to vegetable value
+console.log(fruit); //carrot
